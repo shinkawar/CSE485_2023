fd71b8d59f990532d91530e189c088de360832b7
Thien35 <thiennb01@gmail.com>
2023-02-27 11:45:22 +0700

    ket noi csdl

---
+++ admin/db.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, 'btth01_cse485');
+mysqli_query($conn, "SET NAMES 'utf8'");
+    if(!$conn){
+        echo "Connect failed";
+    }
