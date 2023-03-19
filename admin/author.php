627890fc73b71f3b6a3d1d8963a3e33f21bd6d1c
TamVu2002 <tamvu3130@gmail.com>
2023-03-19 17:55:25 +0700

    lỗi commit

--- admin/author.php
+++ admin/author.php
@@ -1,10 +1,11 @@
 <?php
-    require_once 'db.php';
-   
+require_once 'db.php';
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,6 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style_login.css">
 </head>
+
 <body>
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
@@ -22,26 +24,26 @@
                     <a class="navbar-brand" href="#">Administration</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Trang ngoài</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="author.php">Tác giả</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="article.php">Bài viết</a>
-                    </li>
-                </ul>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../index.php">Trang ngoài</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="author.php">Tác giả</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="article.php">Bài viết</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
@@ -51,9 +53,9 @@
         <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
         <div class="row">
             <div class="col-sm">
-                <a href="add_category.php" class="btn btn-success">Thêm tác giả mới</a>
+                <a href="add_author.php" class="btn btn-success">Thêm tác giả mới</a>
                 <table class="table">
-                <thead>
+                    <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th class="col-sm-3" scope="col">Tên tác giả</th>
@@ -63,25 +65,24 @@
                     </thead>
                     <tbody>
                         <?php
-                            $sql = "SELECT * FROM tacgia;";
-                            $result = mysqli_query($conn, $sql);
-                            $count =0;
-                            $resultCheck = mysqli_num_rows($result);        
-                            if($resultCheck > 0)
-                            {
-                                while($row = mysqli_fetch_assoc($result)){
-                                    $count ++;
+                        $sql = "SELECT * FROM tacgia;";
+                        $result = mysqli_query($conn, $sql);
+                        $count = 0;
+                        $resultCheck = mysqli_num_rows($result);
+                        if ($resultCheck > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                $count++;
                         ?>
-                        <tr>
-                            <td><?= $count?></td>
-                            <td><?= $row['ten_tgia']?></td>
-                            <td>
-                                <a href="edit_category.php?id=2"><i class="fa-solid fa-pen-to-square"></i></a>
-                            </td>
-                            <td>
-                            <a href="delete_category_process.php?id=<?= $row['ma_tgia']?>" onclick="return confirm('Xác nhận xóa?');"><i class="fa-solid fa-trash"></i></a>
-                            </td>
-                        </tr>
+                                <tr>
+                                    <td><?= $count ?></td>
+                                    <td><?= $row['ten_tgia'] ?></td>
+                                    <td>
+                                        <a href="edit_category.php?id=2"><i class="fa-solid fa-pen-to-square"></i></a>
+                                    </td>
+                                    <td>
+                                        <a href="delete_category_process.php?id=<?= $row['ma_tgia'] ?>" onclick="return confirm('Xác nhận xóa?');"><i class="fa-solid fa-trash"></i></a>
+                                    </td>
+                                </tr>
                         <?php
                             }
                         }
@@ -96,4 +97,5 @@
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
+
 </html>
